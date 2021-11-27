c988b8aadc0697e003adbe3e34dae4a4b8b1d3fc
Berkay Gumus <berkaygumus32@gmail.com>
2021-11-27 11:38:25 -0700

    Workshop assignment 4

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '602073127514809',
+        clientSecret: '54f3f0c8972549d9ba561912cdd8a'
+    }
 }
